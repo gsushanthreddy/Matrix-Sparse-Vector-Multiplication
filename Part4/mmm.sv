15ec008a77aa2c7d8a602e5449c3b5f2c5cc1d5a
Akarsh-Gokavarapu <akarsh.gokavarapu.1803@gmail.com>
2024-11-16 16:02:01 -0500

    instantiating of all the modules is done

--- Part4/mmm.sv
+++ Part4/mmm.sv
@@ -16,23 +16,36 @@ module MMM #(
     output OUTPUT_TVALID,
     input OUTPUT_TREADY
 );
+    // logics for counting read address in control logic
     integer m;
     integer n;
     integer k;
 
+    logic clear_m, clear_n, clear_k;
+    logic increment_k, increment_m, increment_n;
+    
+    // logical connections between input_mems and mac_pipe
+    logic signed [INW-1:0] A_data_from_input_mems;
+    logic signed [INW-1:0] B_data_from_input_mems;
+     
+    // logical connections between mac_pipe and fifo_out
+    logic signed [OUTW-1:0]  output_from_mac;
+
+    // logical connections between input_mems and control logic
     logic [$clog2(M*MAXK)-1:0] A_read_addr;
     logic [$clog2(MAXK*N)-1:0] B_read_addr;
     logic matrices_loaded;
+    logic K;
+    logic compute_finished;
+    
+    // logical connections between mac_pipe and control logic
     logic clear_acc;
     logic valid_input;
+
+    // logical connections between fifo_out and control logic
     logic wr_en_fifo;
-    logic compute_finished;
 
-    logic K;
-
-    logic clear_m, clear_n, clear_k;
-    logic increment_k, increment_m, increment_n;
-
+    // initialising FSM
     enum logic [1:0] {mmm_start, read_from_input_memory, store_in_fifo} state, next_state;
   
     always_comb begin
@@ -151,11 +164,78 @@ module MMM #(
             state <= next_state;
         end
     end
+    
+    // Instantiating input memories unit
+    input_mems #(.INW(INW), .M(M), .N(N), .MAXK(MAXK)) input_memories_inst(
+        .clk(clk),
+        .reset(reset),
+        .AXIS_TDATA(INPUT_TDATA),
+        .AXIS_TVALID(INPUT_TVALID),
+        .AXIS_TUSER(INPUT_TUSER),
+        .AXIS_TREADY(INPUT_TREADY),
+        .matrices_loaded(matrices_loaded),
+        .compute_finished(compute_finished),
+        .K(K),
+        .A_read_addr(A_read_addr),
+        .A_data(A_data_from_input_mems),
+        .B_read_addr(B_read_addr),
+        .B_data(B_data_from_input_mems)
 
-    // input_mems, fifo_out and mac_pipe instantiation required
+    );
+
+    //instantiating piplined mac unit
+    mac_pipe #(.INW(INW), .OUTW(OUTW)) pipelined_mac_inst(
+        .in0(A_data_from_input_mems),
+        .in1(B_data_from_input_mems),
+        .valid_input(valid_input),
+        .clear_acc(clear_acc),
+        .reset(reset),
+        .clk(clk),
+        .out(output_from_mac)
+    );
+
+    //instantiating output fifo unit
+    fifo_out #(.OUTW(OUTW), .DEPTH(N)) output_fifo_inst(
+        .clk(clk),
+        .reset(reset),
+        .data_in(output_from_mac),
+        .wr_en(wr_en),
+        .capacity(capacity),
+        .AXIS_TDATA(OUTPUT_TDATA),
+        .AXIS_TVALID(OUTPUT_TVALID),
+        .AXIS_TREADY(OUTPUT_TREADY)
+    );
+
+    // instantiating counter for k
+    incrementk (
+        .clk(clk),
+        .reset(reset),
+        .increment_k(increment_k),
+        .clear_k(clear_k),
+        .k(k)
+    );
+
+    //instantiating counter for m
+    incrementm (
+        .clk(clk),
+        .reset(reset),
+        .increment_m(increment_m),
+        .clear_m(clear_m),
+        .m(m),
+    );
+
+    //instantiating counter for n
+    incrementn (
+        .clk(clk),
+        .reset(reset),
+        .increment_n(increment_n),
+        .clear_n(clear_n),
+        .n(n),
+    );
 
 endmodule
 
+// counter logic for K
 module incrementk(
     input clk, reset,
     input logic increment_k,
@@ -174,6 +254,7 @@ module incrementk(
     end
 endmodule
 
+// counter logic for M
 module incrementm(
     input clk, reset,
     input logic increment_m,
@@ -192,6 +273,7 @@ module incrementm(
     end
 endmodule
 
+// counter logic for N
 module incrementn(
     input clk, reset,
     input logic increment_n,
